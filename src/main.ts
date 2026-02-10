07a5e854c367cd3105c2c619a923e033deaca496
Mikhail Kravtsov <kr.mikki@yandex.ru>
2026-02-10 20:29:19 +0300

    Install dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Lara from '@primeuix/themes/lara'
 
+import './style.css'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+  },
+})
 
 app.mount('#app')
